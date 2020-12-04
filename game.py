be8f55c686a3c4da0e58749c4d476d89bf0cce7b
Eduardo Migueis <57845961+edumigueis@users.noreply.github.com>
2020-12-04 18:36:50 -0300

    Update game.py

--- game.py
+++ game.py
@@ -1,22 +1,26 @@
+"""
+Copyright (c) Eduardo Migueis and Rodrigo Smith 2020.
+"""
 import os
 import sys
 import pygame
 
 pygame.init()
 os.chdir(os.path.dirname(__file__))
-bitSmall = pygame.font.Font(os.getcwd() + '\\assets\\8bitOperatorPlus-Bold.ttf', 20)
+bit_small = pygame.font.Font(
+    os.getcwd() + '\\assets\\8bitOperatorPlus-Bold.ttf', 20)
 bit = pygame.font.Font(os.getcwd() + '\\assets\\8bitOperatorPlus-Bold.ttf', 60)
-bitExtraSmall = pygame.font.Font(os.getcwd() + '\\assets\\8bitOperatorPlus-Bold.ttf', 15)
+bit_extra_small = pygame.font.Font(
+    os.getcwd() + '\\assets\\8bitOperatorPlus-Bold.ttf', 15)
 
 scr_size = (width, height) = (1018, 549)
 width = 1018
 height = 549
 FPS = 60
 
-errMsg = "Pygame display surface not rendering."
+err_msg = "Pygame display surface not rendering."
 
 white = (240, 240, 240)
-background_col = (142, 192, 215)
 
 screen = pygame.display.set_mode(scr_size)
 clock = pygame.time.Clock()
@@ -37,17 +41,18 @@ is_moving_down = True
 shock_x = 1
 points = 0
 
+
 def gameplay():
     playery = 0
     playerx = -80
 
-    gQuit = False
+    gm_quit = False
     screen.blit(bg, (0, 0))
     global is_shooting
     is_shooting = False
-    while not gQuit:
-        if pygame.display.get_surface() == None:
-            print(errMsg)
+    while not gm_quit:  # main loop while user does not quit
+        if pygame.display.get_surface() == None:  # verifies wether the display has been loaded or not
+            print(err_msg)
             return True
         else:
             keys = pygame.key.get_pressed()  # checking pressed keys
@@ -61,8 +66,8 @@ def gameplay():
                     playery += 3.5
 
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    gQuit = True
+                if event.type == pygame.QUIT:  # if user decides to quit, we stop the loop
+                    gm_quit = True
                     return True
 
                 if event.type == pygame.KEYDOWN:
@@ -74,12 +79,12 @@ def gameplay():
                         pygame.event.set_blocked(pygame.KEYDOWN)
                         shoot(playery, playerx + 30)
 
-                    if event.key == pygame.K_ESCAPE:
-                        gQuit = True
+                    if event.key == pygame.K_ESCAPE:  # ESC key detected: back to intro screen
+                        gm_quit = True
                         introScreen()
                         return True
 
-            if not is_shooting:
+            if not is_shooting:  # if the player is not shooting the arrows
                 pygame.event.set_allowed(pygame.KEYDOWN)
                 screen.blit(bg, (0, 0))
                 screen.blit(player, (playerx, playery))
@@ -114,17 +119,17 @@ def gameplay():
                 screen.blit(target, (width - 174, rect_y))
             else:
                 screen.blit(target_lightning, (width - 200 - shock_x, rect_y))
-            
+
             global points
-            pt = bitSmall.render(str(points), True, (250, 250, 250))
+            pt = bit_small.render(str(points), True, (250, 250, 250))
             screen.blit(pt, (width - 75, 20))
 
-            pygame.display.update()
+            pygame.display.update()  # updates display and refreshes state of screen
 
         clock.tick(FPS)
 
 
-def shoot(yStart, xStart):
+def shoot(yStart, xStart):  # method that shoots the arrow and calculates where it landed
     arrowX = xStart + 70
     global is_moving_down
     global rect_y
@@ -132,26 +137,26 @@ def shoot(yStart, xStart):
     global points
     crashed = False
 
-    while arrowX < width:
+    while arrowX < width:  # arrow animation
         screen.blit(bg, (0, 0))
         screen.blit(player_shoot, (xStart - 30, yStart))
         screen.blit(arrow, (arrowX, yStart + 90))
-        pt = bitSmall.render(str(points), True, (250, 250, 250))
+        pt = bit_small.render(str(points), True, (250, 250, 250))
         screen.blit(pt, (width - 75, 20))
 
         arrowX += 20
         shock_x = -shock_x
 
         if is_moving_down == True:
+            rect_y += 2
+            if rect_y > height/7:
+                rect_y += 1
+            if rect_y > height/5:
+                rect_y += 1
+            if rect_y > height/4:
                 rect_y += 2
-                if rect_y > height/7:
-                    rect_y += 1
-                if rect_y > height/5:
-                    rect_y += 1
-                if rect_y > height/4:
-                    rect_y += 2
-                if rect_y > height/3:
-                    rect_y += 2
+            if rect_y > height/3:
+                rect_y += 2
         else:
             rect_y -= 2
 
@@ -168,22 +173,25 @@ def shoot(yStart, xStart):
             screen.blit(target_lightning, (width - 200 - shock_x, rect_y))
 
         pygame.display.update()
- 
+
+        # verifies if arrow has hit the target.
         if arrowX > width - 100 and yStart + 90 > rect_y - 40 and yStart + 90 < rect_y + 150 and crashed == False:
+            # verifies where the arrow hit the target
+            # rect_y is the center of the target image and yStart is the arrow height
             if yStart >= rect_y and yStart < rect_y + 40:
-                points+=10;
+                points += 10
             elif yStart <= rect_y and yStart > rect_y - 30:
-                points+=20;
+                points += 20
             elif yStart <= rect_y and yStart > rect_y - 50:
-                points+=30;
+                points += 30
             elif yStart <= rect_y and yStart > rect_y - 60:
-                points+=40;
+                points += 40
             elif yStart <= rect_y and yStart > rect_y - 70:
-                points+=30;
+                points += 30
             elif yStart <= rect_y and yStart > rect_y - 100:
-                points+=20;
+                points += 20
             elif yStart <= rect_y and yStart > rect_y - 170:
-                points+=10;
+                points += 10
             crashed = True
 
         if arrowX > width - 100:
@@ -191,42 +199,42 @@ def shoot(yStart, xStart):
             is_shooting = False
 
 
-def introScreen():
-    backToMain = False
-    while not backToMain:
+def introScreen():  # loads the intro screen
+    back_to_main = False
+    while not back_to_main:
         global points
         points = 0
         screen.blit(bg, (0, 0))
         if pygame.display.get_surface() == None:
-            print(errMsg)
+            print(err_msg)  # warns user something isn't able to load
             return True
         else:
-            for event in pygame.event.get():
+            for event in pygame.event.get():  # detects keys to play or to leave
                 if event.type == pygame.QUIT:
                     return True
                 if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
                     gameplay()
-                    backToMain = True
+                    back_to_main = True
                 if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                     gameplay()
-                    backToMain = True
-        if pygame.display.get_surface() != None:
+                    back_to_main = True
+        if pygame.display.get_surface() != None:  # UI is shown
             title = bit.render("Archer Game", True, (250, 250, 250))
             screen.blit(title, (width/3 - 25, height/2 - 30))
-            text = bitSmall.render(
+            text = bit_small.render(
                 "Press space or enter to star the game.", True, (250, 250, 250))
             screen.blit(text, (width/3 - 37, height/2 + 60))
-            sub = bitExtraSmall.render(
-                "Points: yellow- 40, red- 30, black- 20, white- 10", True, (250, 250, 250))
+            sub = bit_extra_small.render(
+                "Points: yellow- 40, red- 30, black- 20, white- 10. Space to shoot.", True, (250, 250, 250))
             screen.blit(sub, (20, height - 30))
             pygame.display.update()
         else:
-            print(errMsg)
+            print(err_msg)
 
         clock.tick(FPS)
 
 
-def main():
+def main():  # main method calls intro screen loader
     introScreen()
 
 
